487284d94e1dcc86f3c39c66ea7c54aab75b0dd2
faiq <faiqrazarizvi@gmail.com>
2015-01-07 16:02:55 -0500

    small project to show that browserify-swap isn't working

---
+++ app/yo.js
@@ -0,0 +1,3 @@
+var _ = require('lodash') 
+var backbone = require('backbone')
+document.write(JSON.stringify(_.assign({ 'a': 1 }, { 'b': 2 }, { 'c': 3 })));
--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-var _ = require('underscore')._
+var http = require('http'),
+    fs = require('fs');
+http.createServer(function (req, res) {
+  if (req.url === '/') { 
+    var readStream = fs.createReadStream(__dirname + '/index.html');
+    readStream.pipe(res);  
+  }
+  else {
+    var readStream = fs.createReadStream(__dirname + '/bundle.js');
+    readStream.pipe(res);  
+  }
+}).listen(3000, '127.0.0.1');
+console.log('Server running at http://127.0.0.1:1337/');
